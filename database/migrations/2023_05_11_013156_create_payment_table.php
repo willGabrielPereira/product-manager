1dc92425deefa16e0ed5c8f1d1f978b6096a38f4
willGabrielPereira <will.gabriel.pereira@gmail.com>
2023-05-23 02:18:37 +0000

    Ajuste no composer

--- database/migrations/2023_05_11_013156_create_payment_table.php
+++ database/migrations/2023_05_11_013156_create_payment_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('payment', function (Blueprint $table) {
+        Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
             $table->string('description');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('payment');
+        Schema::dropIfExists('payments');
     }
 };
